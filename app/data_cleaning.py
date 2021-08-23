8e0d908d1232af9bf1d4bc002ea7eac762952d82
Pramod kumar Nagaraj <pramod-kumar.nagaraj@epita.fr>
2021-08-23 19:08:22 +0200

    eda

--- app/data_cleaning.py
+++ app/data_cleaning.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 def data_clean(tickets: pd.DataFrame, cities: pd.DataFrame, stations: pd.DataFrame, providers: pd.DataFrame, distances: pd.DataFrame) -> pd.DataFrame:
+    # section 1
     cities_col = cities[["id", "unique_name"]]
     dataframe = pd.merge(left=tickets, right=cities_col, how='left', left_on="o_city", right_on="id")
     dataframe = dataframe.rename(columns={"unique_name": "city_origin"})
@@ -9,7 +10,8 @@ def data_clean(tickets: pd.DataFrame, cities: pd.DataFrame, stations: pd.DataFra
     dataframe = pd.merge(left=dataframe, right=cities_col, how='left', left_on="d_city", right_on="id")
     dataframe = dataframe.rename(columns={"unique_name": "city_destiny"})
     dataframe = dataframe.drop("id", axis=1)
-
+    
+    #section 2
     stations_col = stations[["id", "unique_name"]]
     dataframe = pd.merge(left=dataframe, right=stations_col, how='left', left_on="o_station", right_on="id")
     dataframe = dataframe.rename(columns={"unique_name": "station_origin"})
@@ -19,15 +21,18 @@ def data_clean(tickets: pd.DataFrame, cities: pd.DataFrame, stations: pd.DataFra
     dataframe = dataframe.rename(columns={"unique_name": "station_destiny"})
     dataframe = dataframe.drop("id", axis=1)
 
+    #section 3
     providers_col = providers[["id", "name", "transport_type"]]
     dataframe = pd.merge(left=dataframe, right=providers_col, how='left', left_on="company", right_on="id")
     dataframe = dataframe.rename(columns={"name": "transport_name", "transport_type": "type"})
     dataframe = dataframe.drop("id", axis=1)
 
+    #section 4
     # dataframe = dataframe.dropna()
     # dataframe["station_distance"] = new_df.apply(lambda x: result[x["station_origin"]][x["station_destiny"]], axis=1)
     dataframe["cities_distance"] = dataframe.apply(lambda x: distances[x["city_origin"]][x["city_destiny"]], axis=1)
 
+    #section 5
     dataframe['departure_ts']= pd.to_datetime(dataframe['departure_ts']) 
     dataframe['arrival_ts']= pd.to_datetime(dataframe['arrival_ts'])
     travel_time = dataframe.arrival_ts - dataframe.departure_ts
